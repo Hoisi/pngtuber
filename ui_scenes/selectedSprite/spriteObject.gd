43b7c5b55422346bc502b15cf2603d573ece5215
iandoesallthethings <iandoesallthethings@gmail.com>
2024-05-04 20:22:25 -0400

    Get midi sync working at all.

--- ui_scenes/selectedSprite/spriteObject.gd
+++ ui_scenes/selectedSprite/spriteObject.gd
@@ -32,7 +32,7 @@ var imageSize = Vector2.ZERO
 #Visuals
 var mouseOffset = Vector2.ZERO
 var grabDelay = 0
-var size = Vector2(1,1)
+var size = Vector2(1, 1)
 
 var showOnTalk = 0
 var showOnBlink = 0
@@ -43,12 +43,13 @@ var z = 0
 var heldTicks = 0
 var dragSpeed = 0
 
-
 #Origin
 var origTick = 0
 var offset = Vector2.ZERO
 
 #Wobble
+var use_midi_wobble = true  # TODO: Add to settings
+
 var xFrq = 0.0
 var xAmp = 0.0
 
@@ -61,7 +62,7 @@ var rLimitMax = 180
 var rLimitMin = -180
 
 #Layer
-var costumeLayers = [1,1,1,1,1,1,1,1,1,1]
+var costumeLayers = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
 #Stretch
 var stretchAmount = 0.0
@@ -82,10 +83,10 @@ var tick = 0
 #Vis toggle
 var toggle = "null"
 
+
 func _ready():
-	
 	Global.main.spriteVisToggles.connect(visToggle)
-	
+
 	var img = Image.new()
 	var err = img.load(path)
 	if err != OK:
@@ -103,22 +104,21 @@ func _ready():
 				print_debug("Failed to load image.")
 				queue_free()
 				return
-		
+
 	var texture = ImageTexture.new()
 	texture = ImageTexture.create_from_image(img)
-	
-	
+
 	tex = texture
 	imageData = img
-	
+
 	imageSize = img.get_size()
-	
+
 	sprite.texture = tex
-	
+
 	var bitmap = BitMap.new()
 	bitmap.create_from_image_alpha(imageData)
-	
-	var polygons = bitmap.opaque_to_polygons(Rect2(Vector2(0, 0), bitmap.get_size()),4.0) #bitmap.get_size()
+
+	var polygons = bitmap.opaque_to_polygons(Rect2(Vector2(0, 0), bitmap.get_size()), 4.0)  #bitmap.get_size()
 
 	var b = false
 	for polygon in polygons:
@@ -126,28 +126,27 @@ func _ready():
 		var collider = CollisionPolygon2D.new()
 		collider.polygon = polygon
 		grabArea.add_child(collider)
-		
+
 		var outline = outlineScene.instantiate()
 		outline.points = polygon
 		outline.add_point(outline.points[0])
 		grabArea.add_child(outline)
-	
+
 	size = imageData.get_size()
-	grabArea.position = size*-0.5
-	
+	grabArea.position = size * -0.5
+
 	sprite.offset = offset
-	
-	grabArea.position = (size*-0.5) + offset
-	
+
+	grabArea.position = (size * -0.5) + offset
+
 	changeFrames()
 	setZIndex()
-	
+
 	if frames > 1:
 		remakePolygon()
 	if !b:
 		remakePolygon()
-	
-	
+
 	add_to_group(str(id))
 	await get_tree().create_timer(0.1).timeout
 	if parentId != null:
@@ -156,49 +155,58 @@ func _ready():
 		nodes[0].sprite.add_child(self)
 		parentSprite = nodes[0]
 		set_owner(nodes[0].sprite)
-	
+
 	setClip(clipped)
-	
-	
+
 	if Global.filtering:
 		sprite.texture_filter = 2
-	
+
+
+func _input(event):
+	if use_midi_wobble and event is InputEventMIDI:
+		match event.message:
+			MIDIMessage.MIDI_MESSAGE_START:
+				tick = 0
+			MIDIMessage.MIDI_MESSAGE_STOP:
+				tick = 0
+			MIDIMessage.MIDI_MESSAGE_TIMING_CLOCK:
+				tick += 1
+
+
 func replaceSprite(pathNew):
 	var img = Image.new()
 	var err = img.load(pathNew)
 	if err != OK:
-		#Runs if image import fails. 
+		#Runs if image import fails.
 		Global.epicFail(err)
 		print_debug("Failed to load image.")
 		return
-	
+
 	path = pathNew
-	
+
 	var texture = ImageTexture.new()
 	texture = ImageTexture.create_from_image(img)
-	
-	
+
 	tex = texture
 	imageData = img
-	
-	
+
 	sprite.texture = tex
-	
+
 	var bitmap = BitMap.new()
 	bitmap.create_from_image_alpha(imageData)
-	
+
 	var polygons = bitmap.opaque_to_polygons(Rect2(Vector2(0, 0), bitmap.get_size()))
-	
+
 	for i in grabArea.get_children():
 		i.queue_free()
-	
+
 	var b = false
 	for polygon in polygons:
 		b = true
 		var collider = CollisionPolygon2D.new()
 		collider.polygon = polygon
 		grabArea.add_child(collider)
-	
+
 		var outline = outlineScene.instantiate()
 		outline.points = polygon
 		outline.add_point(outline.points[0])
@@ -206,47 +214,49 @@ func replaceSprite(pathNew):
 	size = imageData.get_size()
 
 	sprite.offset = offset
-	
-	grabArea.position = (size*-0.5) + offset
-	
+
+	grabArea.position = (size * -0.5) + offset
+
 	if !b:
 		remakePolygon()
 
+
 func _process(delta):
-	tick += 1
+	if not use_midi_wobble:
+		tick += 1
+
 	if Global.heldSprite == self:
-		
 		grabArea.visible = true
 		originSprite.visible = true
-		
+
 	else:
 		grabArea.visible = false
 		originSprite.visible = false
-	
+
 	var glob = dragger.global_position
 	if ignoreBounce:
 		glob.y -= Global.main.bounceChange
-	
+
 	drag(delta)
 	wobble()
-	
-	var length = (glob.y - dragger.global_position.y)
-	
-	rotationalDrag(length,delta)
-	stretch(length,delta)
-	
+
+	var length = glob.y - dragger.global_position.y
+
+	rotationalDrag(length, delta)
+	stretch(length, delta)
+
 	if grabDelay > 0:
 		grabDelay -= 1
-	
+
 	talkBlink()
-	
+
 	animation()
 
+
 func animation():
-	
-	var speed = max(float(animSpeed),Engine.max_fps*6.0)
+	var speed = max(float(animSpeed), Engine.max_fps * 6.0)
 	if animSpeed > 0 and frames > 1:
-		if Global.animationTick % int((speed)/float(animSpeed)) == 0:
+		if Global.animationTick % int((speed) / float(animSpeed)) == 0:
 			if sprite.frame == frames - 1:
 				sprite.frame = 0
 			else:
@@ -254,17 +264,23 @@ func animation():
 	if frames > 1:
 		remakePolygon()
 
+
 func setZIndex():
 	sprite.z_index = z
 
+
 func talkBlink():
 	var faded = 0.2 * int(Global.main.editMode)
-	var value = (showOnTalk + (showOnBlink*3)) + (int(Global.speaking)*10) + (int(Global.blink)*20)
-	var yes = [0,10,20,30,1,21,12,32,3,13,4,15,26,36,27,38].has(int(value))
-	sprite.self_modulate.a = max(int(yes),faded)
+	var value = (
+		(showOnTalk + (showOnBlink * 3)) + (int(Global.speaking) * 10) + (int(Global.blink) * 20)
+	)
+	var yes = [0, 10, 20, 30, 1, 21, 12, 32, 3, 13, 4, 15, 26, 36, 27, 38].has(int(value))
+	sprite.self_modulate.a = max(int(yes), faded)
+
 
 func delete():
 	queue_free()
+
 
 func _physics_process(delta):
 	if Global.heldSprite == self:
@@ -276,33 +292,36 @@ func _physics_process(delta):
 	else:
 		set_physics_process(false)
 
+
 func pressingDirection():
 	var dir = Vector2.ZERO
-	
+
 	dir.x = Input.get_action_strength("move_left") - Input.get_action_strength("move_right")
 	dir.y = Input.get_action_strength("move_up") - Input.get_action_strength("move_down")
 	return dir
-	
+
+
 func moveSprite(dir):
 	if dir != Vector2.ZERO:
 		heldTicks += 1
 	else:
 		heldTicks = 0
-	
+
 	if heldTicks > 30 or heldTicks == 1:
 		var multiplier = 2
 		if heldTicks == 1:
 			multiplier = 1
 		position -= dir * multiplier
-	
-	position = Vector2(int(position.x),int(position.y))
+
+	position = Vector2(int(position.x), int(position.y))
+
 
 func moveOrigin(dir):
 	if dir != Vector2.ZERO:
 		origTick += 1
 	else:
 		origTick = 0
-	
+
 	if origTick > 30 or origTick == 1:
 		var multiplier = 2
 		if origTick == 1:
@@ -310,45 +329,67 @@ func moveOrigin(dir):
 
 		offset += dir * multiplier
 		position -= dir * multiplier
-		
-	offset = Vector2(int(offset.x),int(offset.y))
-	
+
+	offset = Vector2(int(offset.x), int(offset.y))
+
 	sprite.offset = offset
-	grabArea.position = (size*-0.5) + offset
+	grabArea.position = (size * -0.5) + offset
+
 
 func drag(delta):
 	if dragSpeed == 0:
 		dragger.global_position = wob.global_position
 	else:
-		dragger.global_position = lerp(dragger.global_position,wob.global_position,1/dragSpeed)
+		dragger.global_position = lerp(dragger.global_position, wob.global_position, 1 / dragSpeed)
 		dragOrigin.global_position = dragger.global_position
 
+
 func wobble():
-	wob.position.x = sin(tick*xFrq)*xAmp
-	wob.position.y = sin(tick*yFrq)*yAmp
+	if use_midi_wobble:
+		midi_wobble()
+	else:
+		free_wobble()
 
-func rotationalDrag(length,delta):
-	var yvel = (length * rdragStr)
-	
+
+func free_wobble():
+	wob.position.x = sin(tick * xFrq) * xAmp
+	wob.position.y = sin(tick * yFrq) * yAmp
+
+
+func midi_wobble():
+	var ppqn = 24
+	var progress = float(tick) / ppqn
+	var rads = progress * 2 * PI
+	wob.position.x = cos(rads * xFrq * 10) * xAmp
+	wob.position.y = cos(rads * yFrq * 10) * yAmp
+
+
+func rotationalDrag(length, delta):
+	var yvel = length * rdragStr
+
 	#Calculate Max angle
-	
-	yvel = clamp(yvel,rLimitMin,rLimitMax)
-	
-	sprite.rotation = lerp_angle(sprite.rotation,deg_to_rad(yvel),0.25)
 
-func stretch(length,delta):
-	var yvel = (length * stretchAmount * 0.01)
-	var target = Vector2(1.0-yvel,1.0+yvel)
-	
-	sprite.scale = lerp(sprite.scale,target,0.5)
+	yvel = clamp(yvel, rLimitMin, rLimitMax)
+
+	sprite.rotation = lerp_angle(sprite.rotation, deg_to_rad(yvel), 0.25)
+
+
+func stretch(length, delta):
+	var yvel = length * stretchAmount * 0.01
+	var target = Vector2(1.0 - yvel, 1.0 + yvel)
+
+	sprite.scale = lerp(sprite.scale, target, 0.5)
+
 
 func changeCollision(enable):
 	grabArea.monitorable = enable
 	grabArea.monitorable = enable
 
+
 func changeFrames():
 	sprite.hframes = frames
 	sprite.frame = 0
+
 
 func remakePolygon():
 	if remadePolygon:
@@ -357,35 +398,37 @@ func remakePolygon():
 		c.queue_free()
 	var collider = CollisionShape2D.new()
 	var shape = RectangleShape2D.new()
-	shape.size = Vector2(imageSize.y,imageSize.y)
+	shape.size = Vector2(imageSize.y, imageSize.y)
 	collider.shape = shape
-	collider.position = Vector2(imageSize.x,imageSize.y) * Vector2(0.5,0.5)
+	collider.position = Vector2(imageSize.x, imageSize.y) * Vector2(0.5, 0.5)
 	grabArea.add_child(collider)
-	
+
 	var p = imageSize.y * 0.5
 	var outline = outlineScene.instantiate()
-	outline.add_point(Vector2(-p,-p))
-	outline.add_point(Vector2(p,-p))
-	outline.add_point(Vector2(p,p))
-	outline.add_point(Vector2(-p,p))
-	outline.add_point(Vector2(-p,-p))
+	outline.add_point(Vector2(-p, -p))
+	outline.add_point(Vector2(p, -p))
+	outline.add_point(Vector2(p, p))
+	outline.add_point(Vector2(-p, p))
+	outline.add_point(Vector2(-p, -p))
 	outline.position = collider.position
 	grabArea.add_child(outline)
-	
+
 	remadePolygon = true
-	
-func setClip(toggle):
-	if toggle:
+
+
+func setClip(should_clip):
+	if should_clip:
 		sprite.clip_children = CLIP_CHILDREN_AND_DRAW
-		
+
 		for node in getAllLinkedSprites():
 			node.z = z
 			node.setZIndex()
-		
+
 	else:
 		sprite.clip_children = CLIP_CHILDREN_DISABLED
-		
-	clipped = toggle
+
+	clipped = should_clip
+
 
 func getAllLinkedSprites():
 	var nodes = get_tree().get_nodes_in_group("saved")
@@ -395,9 +438,11 @@ func getAllLinkedSprites():
 			linkedSprites.append(node)
 	return linkedSprites
 
+
 func visToggle(keys):
 	if keys.has(toggle):
 		$WobbleOrigin/DragOrigin.visible = !$WobbleOrigin/DragOrigin.visible
+
 
 func makeVis():
 	$WobbleOrigin/DragOrigin.visible = true
